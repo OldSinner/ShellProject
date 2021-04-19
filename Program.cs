95f90681b12e34626f2506791115ba8e588f099e
OldSinner <antylifepatia@gmail.com>
2021-04-19 11:52:20 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShellProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
