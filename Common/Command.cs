b5f3d5e13903f40193c8346f5ce1676ff6772634
OldSinner <antylifepatia@gmail.com>
2021-04-19 13:07:23 +0200

    Configure Command List

---
+++ Common/Command.cs
@@ -0,0 +1,8 @@
+namespace ShellProject.Common
+{
+    public class Command
+    {
+        public string alias { get; set; }
+        public string exePath { get; set; }
+    }
+}
--- Shell.cs
+++ Shell.cs
@@ -2,18 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using ShellProject.Common;
 
 namespace ShellProject
 {
     public class Shell
     {
-        Dictionary<string, string> Commands = new Dictionary<string, string>();
+        List<Command> Commands = new List<Command>();
+        string path;
         string[] welcomeMessage;
         public int Configure()
         {
             try
             {
+
+                path = Directory.GetCurrentDirectory();
                 welcomeMessage = File.ReadAllLines(Directory.GetCurrentDirectory() + "/Configure/WelcomeMessage.txt");
+                var jsonConfigFile = File.ReadAllText(Directory.GetCurrentDirectory() + "/Configure/commandConfig.json");
+                Commands = JsonConvert.DeserializeObject<List<Command>>(jsonConfigFile);
                 return 0;
             }
             catch (Exception x)
@@ -33,24 +42,22 @@ namespace ShellProject
             }
             do
             {
-                Console.Write("$> ");
+                Console.Write(path + "> ");
                 input = Console.ReadLine();
                 Execute(input);
             } while (input != "exit");
         }
         public int Execute(string input)
         {
-            if (Commands.ContainsKey(input))
+            if (Commands.Exists(x => x.alias == input))
             {
                 var process = new Process();
-                process.StartInfo = new ProcessStartInfo(Commands[input])
+                process.StartInfo = new ProcessStartInfo(Commands.Find(x => x.alias == input).exePath)
                 {
                     UseShellExecute = false
                 };
-
                 process.Start();
                 process.WaitForExit();
-
                 return 0;
             }
 
